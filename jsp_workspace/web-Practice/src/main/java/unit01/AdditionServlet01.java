5281426054bcfb93f931e699e14a2ff9b28a4ff7
Min <57902790+y0y00700@users.noreply.github.com>
2024-09-21 21:34:37 +0900

    JSP Review JSP imbeded Object exercise

--- jsp_workspace/web-Practice/src/main/java/unit01/AdditionServlet01.java
+++ jsp_workspace/web-Practice/src/main/java/unit01/AdditionServlet01.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.Session;
+
 /**
  * Servlet implementation class AdditionServlet01
  */
